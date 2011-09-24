884c66e8479be1d51d9f1ce4e941bcf5f661d8fa
jenhan <jenhan@ubuntu.(none)>
2011-09-23 20:21:17 -0700

    initial commit

---
+++ NewsWebBrowser/src/newswebbrowser/NewsWebBrowser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newswebbrowser;
+
+/**
+ *
+ * @author jenhan
+ */
+public class NewsWebBrowser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
